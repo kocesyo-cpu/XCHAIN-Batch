b1b1ca92d47c5507ab772736e480bc085b5dc131
kocesyo-cpu <kocesyo@gmail.com>
2025-10-29 20:33:25 +0700

    Update walletService.ts

--- src/services/walletService.ts
+++ src/services/walletService.ts
@@ -1,4 +1,4 @@
-import { Aptos, AptosConfig, Network, AccountAddress } from '@aptos-labs/aptos';
+import { Aptos, AptosConfig, Network, AccountAddress } from 'aptos';
 import { Token } from '../types';
 
 const aptosConfig = new AptosConfig({ network: Network.TESTNET });
